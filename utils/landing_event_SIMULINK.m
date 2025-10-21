16d7dec73deb411b5cdc7c715ade822b16b3829c
BoolinScoolin <colinriba@ufl.edu>
2025-10-21 12:31:17 -0400

    'reorg'

---
+++ utils/landing_event_SIMULINK.m
@@ -0,0 +1,8 @@
+function [value, isterminal, direction] = landing_event_SIMULINK(~, u)
+% landingEvent - ODE solver event to stop integration when pitch is -0.1 rad
+
+    value = atan2( u(4) , u(2) ) + 0.1; % pitch plus 0.1 rad offset
+    isterminal = 1; % stop the integration when value = 0
+    direction = -1; % only detect value = 0 when altitude is decreasing
+
+end
---
+++ utils/predict_apogee_SIMULINK.m
@@ -0,0 +1,16 @@
+function apogee = predict_apogee_SIMULINK(airbrakeDragCharacteristics,ic)
+% Propagates vehicle flight and returns vehicle apogee
+
+% max time span of flight simulation
+tspan = linspace(0, 200, 1000)';
+
+% Solver
+options = odeset('RelTol', 1e-4, 'Events', @landing_event_SIMULINK);
+[~, u] = ode45(@(t,u) rocket_2dof_eom_SIMULINK(t,u,airbrakeDragCharacteristics) , tspan, ic, options);
+
+%% Organize Data
+u = u*3.28084;  % convert to [ft]
+
+% Extract varibles
+apogee = max(u(:,3));  % y distance  (altitude)
+
